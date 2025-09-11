57296f9f70d43a66df3754a476b0fabfc367d6dd
Anirudh Pabbaraju <125759055+Anirudh-starhash@users.noreply.github.com>
2025-09-11 17:10:44 +0530

    V1 CASGE-BOT UI

--- AI-Wrapper-Frontend/src/app/app.component.ts
+++ AI-Wrapper-Frontend/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ThemeService } from './services/theme.service'; // Import the service
 
 @Component({
   selector: 'app-root',
@@ -17,8 +16,4 @@ import { ThemeService } from './services/theme.service'; // Import the service
 })
 export class AppComponent {
   title = 'AI-Wrapper-Frontend';
-
-  constructor(private themeService: ThemeService) {
-    this.themeService.initializeTheme(); // Initialize the theme on app start
-  }
 }
