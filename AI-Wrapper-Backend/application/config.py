a0f4c0b0929ca5d5a12f4751687da8a198014b4f
Anirudh Pabbaraju <125759055+Anirudh-starhash@users.noreply.github.com>
2025-09-13 21:04:28 +0530

    setup celery worker efficiently

--- AI-Wrapper-Backend/application/config.py
+++ AI-Wrapper-Backend/application/config.py
@@ -12,7 +12,9 @@ class Config():
     CELERY = dict(
         broker_url="redis://localhost:6379/0",
         result_backend="redis://localhost:6379/0",
-        broker_connection_retry_on_startup=True
+        broker_connection_retry_on_startup=True,
+        
+        include=["application.tasks"]
     )
     
 class LocalDevelopmentConfig(Config):
